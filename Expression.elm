d0516da89582b79ff9d9bc3eb6c5c526a855fa4f
Izaak Meckler <ihmeckler@gmail.com>
2015-07-25 17:16:05 -0400

    moved derivative from Calculus to Expression. deleted calculus

--- Expression.elm
+++ Expression.elm
@@ -49,13 +49,13 @@ evaluate expr env =
       maybeMap2 (+) (evaluate e1 env) (evaluate e2 env)
 
     LogBase b e1 ->
-      Maybe.map (logBase b) (evaluate env e1)
+      Maybe.map (logBase b) (evaluate e1 env)
 
     Exp b e1 ->
-      Maybe.map (\x -> b ^ x) (evaluate env e1)
+      Maybe.map (\x -> b ^ x) (evaluate e1 env)
 
     Pow e1 p ->
-      Maybe.map (\x -> x ^ p) (evaluate env e1)
+      Maybe.map (\x -> x ^ p) (evaluate e1 env)
 
 evaluateExn : Expression -> (Environment -> Float)
 evaluateExn expr env =
@@ -67,17 +67,43 @@ evaluateExn expr env =
       c
 
     Mul e1 e2 ->
-      evaluate e1 env * evaluate e2 env
+      evaluateExn e1 env * evaluateExn e2 env
 
     Add e1 e2 ->
-      evaluate e1 env + evaluate e2 env
+      evaluateExn e1 env + evaluateExn e2 env
 
     LogBase b e1 ->
-      logBase b (evaluate env e1)
+      logBase b (evaluateExn e1 env)
 
     Exp b e1 ->
-      b ^ (evaluate env e1)
+      b ^ (evaluateExn e1 env)
 
     Pow e1 p ->
-      (evaluate env e1) ^ p
+      (evaluateExn e1 env) ^ p
+
+derivative : Expression -> String -> Expression
+derivative expr x =
+  case expr of
+    Var y ->
+      if y == x then Constant 1 else Constant 0
+
+    Constant _ -> Constant 0
+
+    Mul e1 e2 ->
+      Add (Mul e1 (derivative e2)) (Mul (derivative e1) e2)
+
+    Add e1 e2 ->
+      Add (derivative e1) (derivative e2)
+
+    LogBase b e1 ->
+      let d = derivative e1 in
+      Mul d (Pow (Mul (Constant (logBase e b)) e1) (-1))
+
+    Exp b e1 ->
+      let d = derivative e1 in
+      Mul (Constant (logBase e b)) (Mul d expr)
+
+    Pow e1 p ->
+      Mul (derivative e1)
+        (Mul (Constant p) (Pow e1 (p - 1)))
 
